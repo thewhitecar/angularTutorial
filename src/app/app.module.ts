5bfad38c050dc2f5ea146b874e7af7753669864f
Ryan Whitecar <ryanwhitecar@gmail.com>
2019-03-03 14:16:27 -0700

    star ratings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
