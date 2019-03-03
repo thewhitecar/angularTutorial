a53d85e1aeeca8f6ec944d74f9eecc75b58a0128
Ryan Whitecar <ryanwhitecar@gmail.com>
2019-03-03 16:28:21 -0700

    added http service for retreiving product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms'
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -8,9 +9,11 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductService } from './products/product.service'
 
+
+
 @NgModule({
   declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent],
   providers: [ProductService]
 })
